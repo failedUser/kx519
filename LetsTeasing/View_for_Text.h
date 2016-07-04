a09c2ce6a6eed9ad622a0c5df532a10693fadb17
apple <yyasyya@163.com>
2016-07-04 15:49:07 +0800

    update test

---
+++ LetsTeasing/View_for_Text.h
@@ -0,0 +1,13 @@
+//
+//  View_for_Text.h
+//  LetsTeasing
+//
+//  Created by apple on 16/7/4.
+//  Copyright © 2016年 yueyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View_for_Text : UIView
+
+@end
---
+++ LetsTeasing/View_for_Text.m
@@ -0,0 +1,21 @@
+//
+//  View_for_Text.m
+//  LetsTeasing
+//
+//  Created by apple on 16/7/4.
+//  Copyright © 2016年 yueyin. All rights reserved.
+//
+
+#import "View_for_Text.h"
+
+@implementation View_for_Text
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
